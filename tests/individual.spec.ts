c77ce91cc12dc6f095d4c29d72ef435400d5b96d
skyboy58568 <chisreerag@gmail.com>
2025-07-02 10:29:47 +0100

    Implement select test

--- tests/individual.spec.ts
+++ tests/individual.spec.ts
@@ -1,8 +1,29 @@
-import test from "node:test";
+import test, { expect } from "@playwright/test"
+
 
 test.describe("Test NEO Page" , () => {
 
-    test("Clicking on Accordion  shows details",()=>{
+    test.beforeEach(async({page}) =>{
+
+        await page.goto("/")
+        await page.waitForSelector(":has-text('Near-Earth Object (NEO) APIs')", {state : "visible"})
+        page.getByText("START").click()
+        await page.waitForTimeout(3000)
+        page.getByRole("link", {name: "View in Explorer"}).first().click()
+        await page.waitForTimeout(3000)
+
+    })
+
+    test("Clicking on kilometerh to show meters diameter range graph", async ({page})=>{
+
+        page.getByRole("button", {name: "Dimensions"}).click()
+        await page.getByRole('combobox', { name: 'Scale Kilometers' }).click()
+        page.getByRole('option', { name: 'Meters', exact: true }).click()
+        await page.waitForTimeout(1000)
+        await page.waitForSelector(":has-text('meters')")
+        let meterItems = page.locator(":has-text('meters')")
+        expect(Array.isArray(meterItems))
+        
 
     })
 
